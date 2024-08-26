ed6c03141797f99c4f16cd56c7c8dc93b6f7fa7f
ZezHok <yulia_1992@mail.ru>
2024-08-26 23:39:04 +0300

    Update main_test.py

--- main_test.py
+++ main_test.py
@@ -12,4 +12,42 @@ def test_update_pet_data(base_url, update_pet_data):
     assert update_pet.headers['Content-Type'] == 'application/json'
 
 
+@pytest.mark.regression
+def test_pet_operations(base_url, pet_id):
 
+    update_data = {
+        "id": pet_id,
+        "category": {
+            "id": 4,
+            "name": "testname"
+        },
+        "name": "lusia",
+        "photoUrls": [
+            "string"
+        ],
+        "tags": [
+            {
+            "id": 6,
+            "name": "testtag"
+            }
+        ],
+        "status": "sold"
+    }
+
+    update_pet = requests.put(f"{base_url}/pet", json=update_data)
+    print("Update pet" + update_pet.text)
+    assert update_pet.status_code == 200
+    print(update_pet.headers)
+    assert update_pet.headers['Content-Type'] == 'application/json'
+
+    get_pet = requests.get(f"{base_url}/pet/{pet_id}")
+    print("Info pet" + get_pet.text)
+    assert get_pet.status_code == 200
+    print(get_pet.headers)
+    assert get_pet.headers['Content-Type'] == 'application/json'
+    pet_info = get_pet.json()
+    assert pet_info['status'] == update_data['status']
+
+def test2():
+    print("second test")
+    
